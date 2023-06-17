770ef6f1cf72a4fa21154378db7af08936d3eba8
Christopher Kenny <ctk44@cornell.edu>
2023-06-16 23:14:36 -0400

    start fn based

--- R/scale_numeric.R
+++ R/scale_numeric.R
@@ -1,12 +1,13 @@
 #' Add a scale for a numeric column
 #'
-#' @param data
-#' @param fn
+#' @param data A `gt` table with class `gt_tbl`
+#' @param fn a function from `scales`
 #'
-#' @return
+#' @return A [gt::gt]
 #' @export
 #'
 #' @examples
+#' #TODO
 gtscale_color_numeric <- function(data, fn) {
 
 }
